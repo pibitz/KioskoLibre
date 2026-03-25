590e1816ad975934e90cb964d456ccfdfa33c293
pibitz <pibi@live.cl>
2026-03-25 20:36:23 -0300

    Initial commit

---
+++ src/main/java/com/educativo/KioskoLibre/KioskoLibreApplication.java
@@ -0,0 +1,13 @@
+package com.educativo.KioskoLibre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskoLibreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskoLibreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educativo/KioskoLibre/KioskoLibreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educativo.KioskoLibre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskoLibreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
